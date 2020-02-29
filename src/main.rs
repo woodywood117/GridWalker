6b64acfaf0bd239dcaa4e2f5854d0e042c420837
James Wood <darkgalac@gmail.com>
2020-02-29 15:36:16 -0500

    Implemented in quicksilver, it's awesome!

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-extern crate glutin_window;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate piston;
-
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderEvent, UpdateArgs, UpdateEvent, PressEvent, Button::Keyboard}; // RenderArgs,
-use piston::window::{WindowSettings};
+use quicksilver::{
+    geom::{Rectangle, Vector},
+    graphics::{Color, Graphics},
+    lifecycle::{run, Event, EventStream, Settings, Window, Key},
+    Result,
+};
 use rand;
-use graphics::*;
 
 const WINDOW_X: f64 = 639.0;
 const WINDOW_Y: f64 = 639.0;
@@ -20,7 +15,7 @@ const MAX_Y: u32 = 40;
 struct Walker {
     x: u32,
     y: u32,
-    color: [f32; 4],
+    color: (u8,u8,u8,f32),
     max_x: u32,
     max_y: u32,
     last_box: u32,
@@ -31,7 +26,7 @@ impl Walker {
         Walker {
             x: rand::random::<u32>() % max_x,
             y: rand::random::<u32>() % max_y,
-            color: [rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>(), 1.0],
+            color: (rand::random::<u8>(), rand::random::<u8>(), rand::random::<u8>(), 1.0),
             max_x,
             max_y,
             last_box: 4,
@@ -59,7 +54,6 @@ struct Grid {
     grid_height: f64,
     grid_count_horizontal: u32,
     grid_count_vertical: u32,
-    //color: [f32; 4],
     boxes: Vec<Walker>,
     past_boxes: std::collections::HashMap<(u32, u32), Walker>,
 }
@@ -83,8 +77,8 @@ impl Grid {
 struct State {
     grid: Grid,
     num_walkers: i32,
-    dt_aggr: f64,
     menu_open: bool,
+    dt_inst: std::time::Instant,
 }
 
 impl State {
@@ -98,7 +92,6 @@ impl State {
                 grid_height: WINDOW_Y,
                 grid_count_horizontal: MAX_X,
                 grid_count_vertical: MAX_Y,
-                //color: [1.0, 1.0, 1.0, 0.2],
                 boxes: vec![
                     Walker::new(MAX_X, MAX_Y),
                     Walker::new(MAX_X, MAX_Y),
@@ -109,8 +102,8 @@ impl State {
                 past_boxes: std::collections::HashMap::new(),
             },
             num_walkers: 5,
-            dt_aggr: 0.0,
             menu_open: false,
+            dt_inst: std::time::Instant::now(),
         };
 
         s
@@ -125,7 +118,6 @@ impl State {
             grid_height: WINDOW_Y,
             grid_count_horizontal: MAX_X,
             grid_count_vertical: MAX_Y,
-            //color: [1.0, 1.0, 1.0, 0.2],
             boxes: vec![],
             past_boxes: std::collections::HashMap::new(),
         };
@@ -146,12 +138,12 @@ impl State {
         }
     }
 
-    fn render_menu(&mut self, _: Context, g: &mut GlGraphics) {
-        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-        clear(BLACK, g);
+    fn render_menu(&mut self, g: &mut Graphics, w: &Window) {
+        g.clear(Color::BLACK);
+        g.present(&w);
     }
 
-    fn render_grid(&mut self, c: Context, g: &mut GlGraphics) {
+    fn render_grid(&mut self, g: &mut Graphics, w: &Window) {
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
         const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
@@ -164,44 +156,38 @@ impl State {
         let past_boxes = self.grid.past_boxes.values().clone();
 
         // Clear the screen.
-        clear(BLACK, g);
-        let transform = c.transform;
+        g.clear(Color::BLACK);
 
         for b in boxes.iter() {
             let x_coord = if b.x == 0 {pos_x} else {pos_x + ((b.x as f64) * bwidth) + ((b.x) as f64) * line_width};
             let y_coord = if b.y == 0 {pos_y} else {pos_y + b.y as f64 * bheight + (b.y) as f64 * line_width};
 
-            let rect = rectangle::rectangle_by_corners(
-                (x_coord - line_width) as f64,
-                (y_coord - line_width) as f64,
-                (x_coord + (bwidth + 2.0 * line_width)) as f64,
-                (y_coord + (bheight + 2.0 * line_width)) as f64);
-            rectangle(RED, rect, transform, g);
-            let rect = rectangle::rectangle_by_corners(
-                (x_coord) as f64,
-                (y_coord) as f64,
-                (x_coord + bwidth) as f64,
-                (y_coord + bheight) as f64);
-            rectangle(b.color, rect, transform, g);
+            // draw outer line
+            let rect = Rectangle::new(Vector::new((x_coord - line_width) as f32, (y_coord - line_width) as f32),
+                                      Vector::new((bwidth + 2.0 * line_width) as f32, (bheight + 2.0 * line_width) as f32));
+            g.fill_rect(&rect, Color::RED);
+
+            // draw inner box
+            let rect = Rectangle::new(Vector::new(x_coord as f32, y_coord as f32),
+                                      Vector::new(bwidth as f32, bheight as f32));
+            g.fill_rect(&rect, Color::from_rgba(b.color.0, b.color.1, b.color.2, b.color.3));
         }
 
+        // draw old boxes
         for  b in past_boxes {
             let x_coord = if b.x == 0 {pos_x} else {pos_x + ((b.x as f64) * bwidth) + ((b.x) as f64) * line_width};
             let y_coord = if b.y == 0 {pos_y} else {pos_y + b.y as f64 * bheight + (b.y) as f64 * line_width};
-            let rect = rectangle::rectangle_by_corners(
-                (x_coord) as f64,
-                (y_coord) as f64,
-                (x_coord + bwidth) as f64,
-                (y_coord + bheight) as f64);
-            rectangle(b.color, rect, transform, g);
+            let rect = Rectangle::new(Vector::new(x_coord as f32, y_coord as f32),
+                                      Vector::new(bwidth as f32, bheight as f32));
+            g.fill_rect(&rect, Color::from_rgba(b.color.0, b.color.1, b.color.2, b.color.3));
         }
+        g.present(&w);
     }
 
-    fn update(&mut self, args: &UpdateArgs) {
-        self.dt_aggr += args.dt;
-        if self.dt_aggr >= (1.0 / 10.0) {
+    fn update(&mut self) {
+        if self.dt_inst.elapsed() > std::time::Duration::from_millis(100) {
             self.grid.update();
-            self.dt_aggr = 0.0;
+            self.dt_inst = std::time::Instant::now();
         }
     }
 
@@ -211,54 +197,47 @@ impl State {
 }
 
 fn main() {
-    // Change this to OpenGL::V2_1 if not working.
-    let opengl = OpenGL::V2_1;
+    run(
+        Settings {
+            size: Vector::new(WINDOW_X as f32, WINDOW_Y as f32).into(),
+            title: "GridWalker",
+            ..Settings::default()
+        },
+        app,
+    );
+}
 
-    // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("GridWalker", [WINDOW_X, WINDOW_Y])
-        .graphics_api(opengl)
-        .samples(2)
-        .resizable(false)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
-
-    // Create a new game and run it.
+async fn app(window: Window, mut gfx: Graphics, mut events: EventStream) -> Result<()> {
     let mut walker = State::new();
-    let gl = &mut GlGraphics::new(opengl);
+    loop {
+        if !walker.menu_open {
+            walker.update();
+        }
 
-    let mut events = Events::new(EventSettings::new());
-    while let Some(e) = events.next(&mut window) {
-        if let Some(args) = e.render_args() {
-            gl.draw(args.viewport(), |c, gl| {
-                if walker.menu_open {
-                    // walker.render_menu(c, gl);
-                    walker.render_grid(c, gl);
-                } else {
-                    walker.render_grid(c, gl);
+        if walker.menu_open {
+            walker.render_grid(&mut gfx, &window);
+        } else {
+            walker.render_grid(&mut gfx, &window);
+        }
+
+        while let Some(e) = events.next_event().await {
+            if let Event::KeyboardInput(key) = e {
+                // println!("{:?}", key);
+                if key.key() == Key::E && key.is_down() {
+                    walker.toggle_menu();
                 }
-            });
-        }
-
-        if let Some(args) = e.update_args() {
-            if !walker.menu_open {
-                walker.update(&args);
-            }
-        }
-
-        if let Some(Keyboard(key)) = e.press_args() {
-            // println!("{:?}", key);
-            if key == piston::Key::E {
-                walker.toggle_menu();
-            }
-            if key == piston::Key::Return {
-                walker.reset_grid();
-            }
-            if key == piston::Key::NumPadPlus || key == piston::Key::Equals {
-                walker.inc_grid_count();
-            }
-            if key == piston::Key::NumPadMinus || key == piston::Key::Minus {
-                walker.dec_grid_count();
+                if key.key() == Key::Return {
+                    walker.reset_grid();
+                }
+                if key.key() == Key::Add || key.key() == Key::Equals {
+                    walker.inc_grid_count();
+                }
+                if key.key() == Key::Subtract || key.key() == Key::Minus {
+                    walker.dec_grid_count();
+                }
+                if key.key() == Key::Escape {
+                   return Ok(());
+                }
             }
         }
     }
